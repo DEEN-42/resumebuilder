1222f8e77ec5684387432a26dcb1adaa6a681330
DEEN-42 <debanshughosh685@gmail.com>
2026-03-26 10:40:58 +0530

    fixed logs

--- backend/index.js
+++ backend/index.js
@@ -139,7 +139,7 @@ const startServer = async () => {
   process.on('SIGINT',  () => { stopScheduler(); process.exit(0); });
 
   server.listen(PORT, () => {
-    console.log(`🚀 Server running on http://localhost:${PORT}`);
+    // console.log(`🚀 Server running on http://localhost:${PORT}`);
     console.log("✅ Redis adapter connected for Socket.IO scaling.");
   });
 };
